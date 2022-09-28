67f6f59c20652038f1f22045c404dd9399ce46df
GryRgrs <102528828+GryRgrs@users.noreply.github.com>
2022-09-28 12:16:37 +0000

    completed first 13 lessons of Jonas course along with assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,53 @@
+//variables
+// let country = "united kingdom";
+// let continent = "europe";
+// let population = 70,000,000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+
+
+//data-types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof true)
+// console.log(typeof 23)
+// console.log(typeof "gary")
+
+// javascriptIsFun = "yes!"
+// console.log(typeof javascriptIsFun)
+
+// let year;
+// console.log(year)
+// console.log(typeof year)
+
+// year = 1991
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let country = "united kingdom";
+// let isIsland = true
+// console.log(isIsland)
+
+
+
+
+//let,const and var
+let age = 30;
+age = 31;
+const birthYear = 1991
+birthYear = 1990
+
+const language = "english"
+console.log(language)
+const language = "french"
+
+
+
+
+
+
